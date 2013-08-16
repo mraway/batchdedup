552d6e01fdb1fcf8de76d11ccf3020707dcd7c19
mikeagun@gmail.com <mikeagun@gmail.com@a3b5a17a-ef7b-3ed6-6d95-b540c4b62eb9>
2013-08-16 19:44:18 +0000

    Updated simulator (CoW now implemented), started workload generator

--- trunk/simulation/BackupScheduler.h
+++ trunk/simulation/BackupScheduler.h
@@ -2,6 +2,13 @@
 
 using namespace std;
 
+extern double total_size(const vector<vector<double> > &machine_loads);
+extern double model_time(const vector<vector<double> > &machine_loads, bool verbose);
+extern double model_vm_time(double vm_load, bool verbose);
+extern double model_cow(double size, double block_dirty_ratio, double backup_time);
+extern double model_unneccessary_cow(double size, double block_dirty_ratio, double backup_time);
+extern double model_round_cow(const vector<vector<double> > &machine_loads);
+
 class BackupScheduler {
     public:
         void setMachineList(std::vector<std::vector<double> > machine_loads);
@@ -17,3 +24,8 @@ class NullScheduler : public BackupScheduler{
         const char * getName();
 };
         
+class CowScheduler : public BackupScheduler{
+    public:
+        bool schedule_round(std::vector<std::vector<double> > &round_schedule);
+        const char * getName();
+};
